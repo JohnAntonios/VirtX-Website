469af7a592bf47fd28774bb0d2248d79626d66e7
Jim Cook <jim.cook@sydney.edu.au>
2018-10-15 09:55:27 +1100

    Fresh commit
    All merged completely and updated.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
-(function() {
-    var containerSelector = '#container360';
-    this.viewer = new Kaleidoscope.Video({
-        source: 'vr.mp4',
-        containerId: containerSelector,
-        height: window.innerHeight,
-        width: window.innerWidth
+$(document).ready(function() {
+    // Members carousel.
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop: true,
+        lazyLoad: true,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            960: {
+                items: 5
+            },
+            1200: {
+                items: 6
+            }
+        }
     });
-    this.viewer.render();
-    window.onresize = function() {
-        this.viewer.setSize({ height: window.innerHeight, width: window.innerWidth });
-    }.bind(this);
-    document.querySelector(containerSelector).addEventListener('touchend', this.viewer.play.bind(this.viewer));
-    document.body.addEventListener('click', function() {
-        this.viewer.play();
-    }.bind(this));
-})();
+
+    // Navbar interaction.
+    $(function () {
+        var lastScrollTop = 0;
+        var $navbar = $('.navbar');
+      
+        $(window).scroll(function(event){
+          var st = $(this).scrollTop();
+          if (st > lastScrollTop) {
+            $navbar.fadeOut();
+          } else {
+            $navbar.fadeIn();
+          }
+          lastScrollTop = st;
+        });
+    });
+});
+
