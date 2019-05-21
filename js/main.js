e32046245549eecc60786a9c6ed2e7e21dbf5abd
Jim Cook <jim.cook@sydney.edu.au>
2019-05-22 09:40:17 +1000

    added Benjy partner!

--- js/main.js
+++ js/main.js
@@ -1,89 +1,95 @@
 $(document).ready(function() {
-    
-    // Members carousel.
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        loop: true,
-        lazyLoad: true,
-        margin: 10,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            960: {
-                items: 5
-            },
-            1200: {
-                items: 6
-            }
-        }
-    });
+	// Members carousel.
+	var owl = $(".owl-carousel");
+	owl.owlCarousel({
+		loop: true,
+		lazyLoad: true,
+		margin: 10,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			960: {
+				items: 5
+			},
+			1200: {
+				items: 6
+			}
+		}
+	});
 
-    // Navbar interaction.
-    $(function () {
-        var lastScrollTop = 0;
-        var $navbar = $('.navbar');
-      
-        $(window).scroll(function(event){
-          var st = $(this).scrollTop();
-          if (st > lastScrollTop) {
-            $navbar.fadeOut();
-          } else {
-            $navbar.fadeIn();
-          }
-          lastScrollTop = st;
-        });
-    });
+	// Navbar interaction.
+	$(function() {
+		var lastScrollTop = 0;
+		var $navbar = $(".navbar");
 
-    //360 video
-    (function() {
-        var containerSelector = '#container360';
-        this.viewer = new Kaleidoscope.Video({
-            source: 'vr.mp4',
-            containerId: containerSelector,
-            height: window.innerHeight,
-            width: window.innerWidth,
-            loop: true
-        });
-        this.viewer.render();
-        window.onresize = function() {
-            this.viewer.setSize({height: window.innerHeight, width: window.innerWidth});
-        }.bind(this);
-        document.querySelector(containerSelector).addEventListener('touchend', this.viewer.play.bind(this.viewer));
-        document.body.addEventListener('onclick', function() {
-            this.viewer.play();
-        }.bind(this));
-    })();
+		$(window).scroll(function(event) {
+			var st = $(this).scrollTop();
+			if (st > lastScrollTop) {
+				$navbar.fadeOut();
+			} else {
+				$navbar.fadeIn();
+			}
+			lastScrollTop = st;
+		});
+	});
 
-    // Arrow click disappear.
-    $('.arrow-clicker').click(function(){
-        $('.arrow-clicker').fadeOut("slow");
-    });
+	//360 video
+	(function() {
+		var containerSelector = "#container360";
+		this.viewer = new Kaleidoscope.Video({
+			source: "assets/vid/vr.mp4",
+			containerId: containerSelector,
+			height: window.innerHeight,
+			width: window.innerWidth,
+			loop: true
+		});
+		this.viewer.render();
+		window.onresize = function() {
+			this.viewer.setSize({
+				height: window.innerHeight,
+				width: window.innerWidth
+			});
+		}.bind(this);
+		document
+			.querySelector(containerSelector)
+			.addEventListener("touchend", this.viewer.play.bind(this.viewer));
+		document.body.addEventListener(
+			"onclick",
+			function() {
+				this.viewer.play();
+			}.bind(this)
+		);
+	})();
 
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+	// Arrow click disappear.
+	$(".arrow-clicker").click(function() {
+		$(".arrow-clicker").fadeOut("slow");
+	});
 
-        if (scroll < 850) {
-            $('.arrow-clicker').fadeIn("slow");
-        } else {
-            $('.arrow-clicker').fadeOut("slow");
-        }
-    });
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
 
-    // Make all member cards equal height.
-    $('.member-container').each(function() {         
-        var highestBox = 0;
+		if (scroll < 850) {
+			$(".arrow-clicker").fadeIn("slow");
+		} else {
+			$(".arrow-clicker").fadeOut("slow");
+		}
+	});
 
-        $('.member-card', this).each(function(){
-            if ($(this).height() > highestBox) {
-                highestBox = $(this).height(); 
-            }
-        });  
-            
-        $('.member-card', this).height(highestBox);
-                    
-    }); 
+	// Make all member cards equal height.
+	$(".member-container").each(function() {
+		var highestBox = 0;
+
+		$(".member-card", this).each(function() {
+			if ($(this).height() > highestBox) {
+				highestBox = $(this).height();
+			}
+		});
+
+		$(".member-card", this).height(highestBox);
+	});
 });
